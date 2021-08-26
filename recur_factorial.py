4a26f16c059d95316e674e0be77a968bd0d79c37
danaelshrbiny10 <https://github.com/danaelshrbiny10/hue_lights.git>
2021-08-26 02:15:22 +0200

    add code

---
+++ recur_factorial.py
@@ -0,0 +1,24 @@
+def recur_factorial(num):
+    if num ==1:
+        return num
+    else:
+        return num * recur_factorial(num-1)
+number = int(input("Enter The Number : "))
+
+if number < 0 :
+    print("Sorry, Factorial doesn't exist for negative numbers")
+
+elif number == 0 :
+    print("The fatorial of 0 is 1")
+
+else:
+    print("The factorial of",number,"is",recur_factorial(number))
+
+
+print("-------------------------------------------------------")
+
+
+##Another Solution :
+import math
+
+print(math.factorial(5))
